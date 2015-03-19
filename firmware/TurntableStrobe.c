2542ad24528b70dc6de60feeb1e11df998cd24fc
shimniok@gmail.com <shimniok@gmail.com>
2015-03-19 10:03:23 -0600

    Reverted to original code. Fixed original makefile for much slower bitclock so I can flash these with LED installed

--- firmware/TurntableStrobe.c
+++ firmware/TurntableStrobe.c
@@ -3,7 +3,7 @@
 #include <util/delay.h>
 
 /**
- * Turntable Strobe driver for ATtiny25
+ * Turntable Strobe driver for ATtiny25 family
  * 
  * @author: Michael Shimniok - www.bot-thoughts.com
  * @created: 3/11/2014
@@ -20,20 +20,21 @@
 // but it's not like the microcontroller has much else to do.
 #define LED  PB2 
 #define PUMP PB0
-#define PERIOD 50					// 12MHz -> 60Hz, 10MHz -> 50Hz
-#define DUTY 30						// duty cycle == (WATCH+1)/(PERIOD+1)
-
-volatile uint8_t count = 0;
+#define ONTIME 40
+#define MATCH 200		// 12MHz/4/200 = 15000
+#ifndef PERIOD
+#error No PERIOD defined, use -DPERIOD=?
+#endif
 
 int main() {
 	DDRB |= (1<<LED)|(1<<PB1);
 	PORTB &= ~(1<<LED); 
 
-	TCCR1 |= (1<<PWM1A)|				// Enables PWM mode based on comparator OCR1A
-			 (1<<CTC1)|					// Clear timer on match with OCR1C
-			 (1<<CS12)|(1<<CS10);		// Run timer at PCK/16
-	OCR1C = PERIOD-1;					// PWM period
-	TIMSK |= (1<<TOIE1);				// Enable interrupt on timer overflow
+	TCCR1 |= (1<<PWM1A)|			// Clear timer on compare match
+			 (1<<CTC1)|				// PWM using OCR1A to count and OCR1C to match;
+			 (1<<CS11)|(1<<CS10);	// Run timer at PCK/4 = 12Mhz/4 = 3MHz;
+	OCR1C = MATCH-1;				// To get N counts, set OCR1C to N-1
+	TIMSK |= (1<<TOIE1);			// Enable interrupt on timer overflow
 	sei();
 	
 	// Run the charge pump, timing doesn't have to be super accurate
@@ -45,11 +46,15 @@ int main() {
 }
 
 ISR(TIM1_OVF_vect) {
-	++count;
-	if (count > DUTY) {
-		PORTB |= (1<<LED);
-	} else if (count > PERIOD) {
+	static unsigned int count = 0;
+	count++;
+
+	if (count >= PERIOD) {
 		count = 0;
-		PORTB &= ~(1<<LED);
+	} else if (count > ONTIME) {
+		PORTB |= (1<<LED);
+	} else {
+		PORTB &= ~(1<<LED); // Toggle LED line
 	}
+
 }
