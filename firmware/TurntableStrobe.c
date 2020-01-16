8cdcb3c153b2a7becc6bfb8e4fb5f048e41a9754
shimniok@gmail.com <shimniok@gmail.com>
2020-01-16 08:35:06 -0700

    Timing parameters added to makefile & source

--- firmware/TurntableStrobe.c
+++ firmware/TurntableStrobe.c
@@ -4,25 +4,25 @@
 
 /**
  * Turntable Strobe driver for ATtiny25 family
- * 
+ *
  * @author: Michael Shimniok - www.bot-thoughts.com
  * @created: 3/11/2014
  * @description: Firmware for TurntableStrobe used to calibrate
  * turntable speed. Most turntables use some form of incandescent bulb
  * running off mains, which means they light up twice per cycle. To
- * emulate this behavior, the firmware pulses an LED at 120Hz (60Hz 
+ * emulate this behavior, the firmware pulses an LED at 120Hz (60Hz
  * mains) or 100Hz (50Hz mains). It also runs a simple Dickson charge
  * pump to "double" voltage for the LED strobe.
- * 
- * For the LED, TIMER1 is used with overflow interrupt to increment a 
+ *
+ * For the LED, TIMER1 is used with overflow interrupt to increment a
  * counter variable for precise timing. Another variable controls the
  * duty cycle. The LED cathode (ground side) is connected to the GPIO
  * pin, so the LED is on when the pin is low (sinking current).
- * 
+ *
  * While I realize there are other ways to do all this (such as running
  * the charge pump off OC1A) this is what I chose for now. No need to
  * overthink things, right? :)
- * 
+ *
  * For more design details see DesignCalcs.ods.
  */
 
@@ -32,15 +32,45 @@
  * 10MHz xtal--no change to firmware (which is significantly more
  * convenient when you're trying to make an sell a bunch of these)
  */
-#define MATCH  25		// See DesignCalcs.ods, TimerCalc tab
-#define PERIOD 250		// See DesignCalcs.ods, TimerCalc tab
-#define ONTIME 80       // LED duty cycle (sharpness vs. brightness)
+
+// We're expecting certain macros defined externally
+#if F_STROBE == 50
+#  if F_XTAL == 10
+#    define PERIOD 125
+#    define ONTIME 40
+#    define MATCH 25
+#  elif F_XTAL == 12
+#    define PERIOD 150
+#    define ONTIME 48
+#    define MATCH 25
+#  elif F_XTAL == 147
+#    define PERIOD 192
+#    define ONTIME 61
+#    define MATCH 24
+#  else
+#    error F_XTAL must be 10, 12, or 147 for 50Hz
+#  endif
+#elif F_STROBE == 60
+#  if F_XTAL == 12
+#    define PERIOD 125
+#    define ONTIME 40
+#    define MATCH 25
+#  elif F_XTAL == 147
+#    define PERIOD 160
+#    define ONTIME 51
+#    define MATCH 24
+#  else
+#    error F_XTAL must be 12 or 147 for 60Hz
+#  endif
+#else
+#  error F_STROBE must be 50 or 60
+#endif
 
 /**
- * I could put one of these on OC1A ... but it's not like the 
+ * I could put one of these on OC1A ... but it's not like the
  * microcontroller has much else to do.
  */
-#define LED  PB2 
+#define LED  PB2
 #define PUMP PB0
 
 /**
@@ -62,11 +92,11 @@ int main() {
 
 	TCCR1 |= (1<<PWM1A)|			// PWM using OCR1A to count and OCR1C to match;
 			 (1<<CTC1)|				// Clear timer on compare match;
-			 (1<<CS11);				// Timer prescaler, see DesignCalcs.ods, TimerCalc tab
+			 (1<<CS11)|(1<<CS10);	// Timer prescaler, see DesignCalcs.ods, TimerCalc tab
 	OCR1C = MATCH-1;				// To get N counts, set OCR1C to N-1
 	TIMSK |= (1<<TOIE1);			// Enable interrupt on timer overflow
 	sei();
-	
+
 	while (1) {
 		_delay_ms(1); // nothing to do here.
 	}
@@ -74,9 +104,9 @@ int main() {
 
 /**
  * This ISR handles the TIMER1 overflow, which is set to a fairly high
- * frequency to allow precise software timing of the actual on and off
+ * Frequency to allow precise software timing of the actual on and off
  * times of the LED (controlled by PERIOD and ONTIME).
- * 
+ *
  * While we're at it, we go ahead and pulse the charge pump, too.
  */
 ISR(TIM1_OVF_vect) {
